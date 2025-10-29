4c2fdc62618d6fb9ccd02b93d0a2ecb51cf84a60
Java Project Dima <cubitai85@gmail.com>
2025-10-29 23:02:09 +0200

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import models.Cars;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+//        Scanner enter = new Scanner(System.in);
+//        System.out.print("Enter tipul de auto: ");
+//        String car3 = enter.nextLine();
+//        System.out.println("Tipul de auto  "+ car3);
+        Cars car1 = new Cars();
+        car1.setTipAuto("Truck");
+        car1.setMarca("Opel");
+        car1.setSeats((byte) 10);
+        System.out.println(car1.getTipAuto() + "  " + car1.getMarca());
+        Cars car2 = new Cars("Bus", "BMW", "Yelow");
+        System.out.println(car2.getTipAuto() + "  " + car2.getMarca() + " " + car2.getColor());
     }
 }
--- src/models/Cars.java
+++ src/models/Cars.java
@@ -1,4 +1,56 @@
 package models;
 
 public class Cars {
+    String tipAuto, marca, color;
+    Float weight;
+    byte seats;
+
+    public Cars() {
+
+    }
+    public Cars (String tipAuto,String marca, String color){
+        this.tipAuto =tipAuto;
+        this.marca = marca;
+        this.color = color;
+    }
+
+    public String getTipAuto() {
+        return tipAuto;
+    }
+
+    public void setTipAuto(String tipAuto ) {
+        this.tipAuto = tipAuto;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public byte getSeats() {
+        return seats;
+    }
+
+    public void setSeats(byte seats) {
+        this.seats = seats;
+    }
 }
