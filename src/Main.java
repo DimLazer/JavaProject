0b823a5779981c6c11b8e48ae222b7f97b4b999b
Java Project Dima <cubitai85@gmail.com>
2025-11-02 16:46:35 +0200

    Change 2

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,6 @@ public class Main {
 
         System.out.println("You can move with your vehicle at max speed :" + car.getMaxSpeed() + "km/h");
         System.out.println("1 "+"You have " + car.getTipAuto() + " de model " + car.getModel() + " care are " + car.getSeats() + " locuri .");
-//        System.out.println("2 "+"You have " + car1.getTipAuto() + " de model " + car1.getModel() + " care are " + car1.getSeats() + " locuri .");
+       System.out.println("2 "+"You have " + car1.getTipAuto() + " de model " + car1.getModel() + " care are " + car1.getSeats() + " locuri .");
     }
 }
