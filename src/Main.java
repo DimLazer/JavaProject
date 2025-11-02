3bb861aab6bf2537b8aab4a1d01861b467375246
Java Project Dima <cubitai85@gmail.com>
2025-11-02 22:37:19 +0200

    add new line

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println(car1.getTipAuto() + "  " + car1.getMarca());
         Cars car2 = new Cars("Bus", "BMW", "Yelow");
         System.out.println(car2.getTipAuto() + "  " + car2.getMarca() + " " + car2.getColor());
+        System.out.println(car2.getTipAuto() + "  " + car2.getMarca() + " " + car2.getColor());
     }
 }
