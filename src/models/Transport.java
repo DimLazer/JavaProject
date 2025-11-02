08a52d102cee608a85c2f95b6c2d523e5e8d98f2
Java Project Dima <cubitai85@gmail.com>
2025-11-02 16:26:34 +0200

    Change 1

---
+++ src/models/Transport.java
@@ -0,0 +1,77 @@
+package models;
+
+public class Transport {
+    String tipAuto, model, color, licensePlate;
+    Float weight;
+    short maxSpeed;
+    byte seats;
+
+    public Transport() {
+
+    }
+
+    public Transport(String tipAuto, String model, byte seats, short maxSpeed, String licensePlate, String color) {
+        this.tipAuto = tipAuto;
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.licensePlate = licensePlate;
+        this.seats = seats;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getTipAuto() {
+        return tipAuto;
+    }
+
+    public short getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(short maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setTipAuto(String tipAuto) {
+        this.tipAuto = tipAuto;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Float weight) {
+        this.weight = weight;
+    }
+
+    public byte getSeats() {
+        return seats;
+    }
+
+    public void setSeats(byte seats) {
+        this.seats = seats;
+    }
+}
