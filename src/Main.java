3f1a126ac89ea04e002b7098133ad00d58dc19d5
Java Project Dima <cubitai85@gmail.com>
2025-11-02 16:27:08 +0200

    Change 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,48 @@
-import models.Cars;
+import models.Transport;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Cars car1 = new Cars();
-        car1.setTipAuto("Truck");
-        car1.setMarca("Opel");
-        car1.setSeats((byte) 10);
-        System.out.println(car1.getTipAuto() + "  " + car1.getMarca());
-        Cars car2 = new Cars("Bus", "BMW", "Yelow");
-        System.out.println(car2.getTipAuto() + "  " + car2.getMarca() + " " + car2.getColor());
+//        Transport car = new Transport();
+//        Transport car1 = new Transport("TRUCK","OPEL",(byte)5,(short)200,"GJJ-567","Black" );
+//        car.setTipAuto("Bus");
+//        car.setModel("IVECO");
+//        car.setLicensePlate("HHJJK-775");
+//        car.setColor("Black");
+//        car.setSeats((byte) 50);
+//        car.setMaxSpeed((short) 120);
+        Scanner enter = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        Transport car = new Transport();
+        for (int i = 1; i <= 2; i++) {
+
+            System.out.print("Enter " + i + " type of transport: ");
+            car.setTipAuto(enter.nextLine());
+
+            System.out.print("Enter " + i + " model of vehicle: ");
+            car.setModel(enter.nextLine());
+
+            System.out.print("Enter " + i + " number of seats: ");
+            car.setSeats(enter.nextByte());
+
+            System.out.print("Enter max speed for the " + i + " transport: ");
+            car.setMaxSpeed(enter.nextShort());
+            enter.nextLine();
+
+            System.out.print("Enter licence number of " + i + " transport: ");
+            car.setLicensePlate(enter.nextLine());
+          //  enter.nextLine();
+
+            String all = "Type of transport: " + car.getTipAuto() + " Model: " + car.getModel() + " Num seats: " + car.getSeats() + "   Speed: " + car.getMaxSpeed()+ " km/h  licence plate: " + car.getLicensePlate();
+            list.add(all);
+            System.out.println(" ✅ Transport was added \n ");
+        }
+
+        System.out.println("You can move with your vehicle at max speed :" + car.getMaxSpeed() + "km/h");
+        System.out.println("1 "+"You have " + car.getTipAuto() + " de model " + car.getModel() + " care are " + car.getSeats() + " locuri .");
+//        System.out.println("2 "+"You have " + car1.getTipAuto() + " de model " + car1.getModel() + " care are " + car1.getSeats() + " locuri .");
     }
 }
